c62061604a605540c53fe39786ef652a543a322a
Georgiana <ailenigeorgiana27@icloud.com>
2025-05-26 19:08:47 +0300

    NBA-League

---
+++ NBA_League/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NBA_League.ui;
+
+namespace NBA_League
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI console = new UI();
+            console.Strart();
+        }
+    }
+}
